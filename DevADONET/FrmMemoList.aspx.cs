5d8480ad91323eb41a40424df47f893a4f64885c
Joon <kweonhj1542@gmail.com>
2020-11-08 16:28:21 +0900

    third commit

--- DevADONET/FrmMemoList.aspx.cs
+++ DevADONET/FrmMemoList.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Configuration;
-using System.Data;
+using System.Data; 
 using System.Data.SqlClient;
 
 namespace DevADONET
